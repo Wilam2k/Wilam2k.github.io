2da3d3a1e1ac35c2dc679038431666f3a3ec9393
Sjöström <william.sjostrom@sogeti.com>
2024-11-12 15:53:23 +0100

    wip

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -43,7 +43,7 @@ function a11yProps(index: number) {
 export const AboutMe = () => {
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     console.log(newValue);
     setValue(newValue);
   };
